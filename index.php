9a203f68f8a0094e5a9d804a3e5eb5351fce25f0
Erica Tollefson <etollefson@cnm.edu>
2019-08-01 13:49:13 -0600

    added navbar v1 to index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 		<!-- Additional CSS -->
+		<link rel="stylesheet" href="styles.css" type="text/css" />
 
 		<!-- jQuery, then Popper.js, then Bootstrap JS-->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -18,7 +19,42 @@
 		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
-		<h1>Hello World!</h1>
+		<div class="container ">
+			<nav class="navbar navbar-expand-lg navbar-light border border-muted rounded" style="background-color: #F8F8F8;">
+				<a class="navbar-brand text-muted" href="#">Challenge</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse text-muted" id="navbarNavAltMarkup">
+					<div class="navbar-nav ml-auto">
+						<a class="nav-item nav-link" href="#">Link 1</a>
+						<a class="nav-item nav-link" href="#">Link 2</a>
+						<a class="nav-item nav-link" href="#">Link 3</a>
+						<a class="nav-item nav-link" href="#">Link 4</a>
+					</div>
+				</div>
+			</nav>
+
+
+
+			<div class="jumbotron">
+
+			</div>
+
+			<div class="about">
+
+			</div>
+
+			<div class="gallery">
+
+			</div>
+
+			<div class="footer">
+
+			</div>
+
+		</div>
+
 	</body>
 
 </html>
